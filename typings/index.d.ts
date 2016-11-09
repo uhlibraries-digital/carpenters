89d7d16f1d98dcad057eb2180c3044dc119b238d
Sean Watkins <slwatkins@uh.edu>
2016-11-09 09:48:32 -0600

    Add dependencies

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,5 @@
+/// <reference path="globals/csv-stringify/index.d.ts" />
 /// <reference path="globals/github-electron/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/xlsx/index.d.ts" />
 /// <reference path="modules/mime/index.d.ts" />
