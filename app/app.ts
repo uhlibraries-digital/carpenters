75904925408c38a72b07309e4ee52196e15a67f4
Sean Watkins <seanlw@gmail.com>
2017-03-17 06:55:13 -0500

    Enable angular production mode when packaged

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,13 @@
 /// <reference path="../typings/index.d.ts" />
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 
 import { AppModule } from './app.module';
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 const platform = platformBrowserDynamic();
+if (process.env.NODE_ENV === 'production') {
+  enableProdMode();
+}
 platform.bootstrapModule(AppModule);
