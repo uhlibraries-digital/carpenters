bd4b9386d937596c24b8a7cd688ebafaf4d9bcd6
Sean Watkins <slwatkins@uh.edu>
2019-09-30 08:33:50 -0500

    Edit production notes in Files view

--- src/app/components/file-view/object-list.component.ts
+++ src/app/components/file-view/object-list.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { Subscription } from "rxjs/Subscription";
 
 import { ArchivesSpaceService } from 'app/services/archivesspace.service';
 import { ObjectService } from 'app/services/object.service';
 import { StandardItemService } from 'app/services/standard-item.service';
 import { SaveService } from 'app/services/save.service';
+import { ElectronService } from 'app/services/electron.service';
+import { ProductionNotesService } from 'app/services/production-notes.service';
 
 @Component({
   selector: 'object-list',
@@ -21,6 +23,9 @@ export class ObjectListComponent implements OnInit, OnDestroy {
     private archivesSpaceService: ArchivesSpaceService,
     private standardItemService: StandardItemService,
     private objectService: ObjectService,
+    private electronService: ElectronService,
+    private note: ProductionNotesService,
+    private changeRef: ChangeDetectorRef,
     private saveService: SaveService
   ) { }
 
@@ -59,6 +64,30 @@ export class ObjectListComponent implements OnInit, OnDestroy {
   onClick(obj: any) {
     this.selectedObject = obj;
     this.objectService.setObject(obj);
+  }
+
+  onContextMenu(index: number, c: any, e: MouseEvent) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    let contextMenu = new this.electronService.Menu();
+    contextMenu.append(new this.electronService.MenuItem({
+      label: (c.productionNotes ? 'Edit' : 'Add') + ' Note',
+      click: () => {
+        this.note.display(c);
+      }
+    }));
+    if (c.productionNotes) {
+      contextMenu.append(new this.electronService.MenuItem({
+        label: 'Clear Note',
+        click: () => {
+          c.productionNotes = '';
+          this.changeRef.detectChanges();
+        }
+      }));
+    }
+
+    contextMenu.popup(this.electronService.remote.getCurrentWindow());
   }
 
   displayTitle(obj: any) {
